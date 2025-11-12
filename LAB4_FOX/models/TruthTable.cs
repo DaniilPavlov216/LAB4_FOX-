6ba22b0fbdbf3ec3a4150df8369d43a0a19d826d
DedushkaDanila <Admin@DESKTOP-9KNLAUP>
2025-11-12 10:17:17 +0300

    это редачьте тут пока тупо каркас и по мелочи

---
+++ LAB4_FOX/models/TruthTable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LAB4_FOX.Models
+{
+    public class TruthTable
+    {
+        public List<TruthTableRow> Rows { get; } = new List<TruthTableRow>();
+        public int VariableCount { get; }
+
+        public TruthTable(int variableCount)
+        {
+            VariableCount = variableCount;
+            GenerateTable();
+        }
+
+        private void GenerateTable()
+        {
+            int rowCount = 1 << VariableCount;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                var inputs = new bool[VariableCount];
+                for (int j = 0; j < VariableCount; j++)
+                {
+                    inputs[j] = ((i >> (VariableCount - 1 - j)) & 1) == 1;
+                }
+                Rows.Add(new TruthTableRow(inputs, false));
+            }
+        }
+    }
+
+    public class TruthTableRow
+    {
+        public bool[] Inputs { get; }
+        public bool Output { get; set; }
+
+        public TruthTableRow(bool[] inputs, bool output)
+        {
+            Inputs = inputs;
+            Output = output;
+        }
+    }
+}
